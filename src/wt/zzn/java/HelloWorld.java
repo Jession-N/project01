abaaf602b57b7803cf1a9348d5e07b1d9b7d40b2
jession-lin <2990818166@qq.com>
2018-12-20 12:12:27 +0800

    上传测试

---
+++ src/wt/zzn/java/HelloWorld.java
@@ -0,0 +1,34 @@
+package wt.zzn.java;
+
+import java.util.ArrayList;
+
+public class HelloWorld {
+//注释
+
+    /**
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        ArrayList array=new ArrayList();
+        array.add("aaaa");
+        array.add("bbb");
+        for (Object o : array) {
+
+        }
+
+        for (int i = 0; i < array.size(); i++) {
+
+        }
+
+        for (int i = array.size() - 1; i >= 0; i--) {
+
+        }
+
+    }
+
+    public void method() {
+
+
+    }
+}
